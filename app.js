4efd1c668d9972f108b63f9e9669c437dcaedf53
Emrys-Hong <hongpengfei.emrys@gmail.com>
2019-02-23 19:02:39 +0800

    landing page - post route to submit email

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); // help to parse the request from the body handler
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
